82b7edac7b1da528324ccb29fa263eb473dd1084
Solo244 <solo244@gmail.com>
2016-09-25 22:08:35 +0200

    Tweet overflow

--- dev/js/main/tweets.js
+++ dev/js/main/tweets.js
@@ -8,4 +8,5 @@ function toggleTweet(){
   $tweets.toggleClass("tweets_active");
   $feed.toggleClass("tweetfeed");
   $overlay.toggleClass("active_overlay");
+  $doc.toggleClass("body_overflow");
 }
