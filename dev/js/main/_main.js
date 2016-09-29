8697190f15de78091dabb66eb7092a3634c78831
Ken Van Damme <solo244@gmail.com>
2016-09-29 13:42:43 +0200

    Added some new content

--- dev/js/main/_main.js
+++ dev/js/main/_main.js
@@ -1,10 +1,13 @@
 /*
  * Vars
  */
-var $doc = $("html, body"),
-    $tweets = $doc.find(".tweets"),
+var $doc = $("html, body"), // General
+    $tweets = $doc.find(".tweets"), // Twitter
     $tweets_btn = $tweets.find(".twit"),
-    $overlay = $doc.find(".tweets_overlay");
+    $overlay = $doc.find(".tweets_overlay"),
+    $project = $doc.find(".project"), // Projects
+    $project_grid = $project.find(".projects_portfolio"),
+    $project_grid_website = $project_grid.find(".project_website");
 
 /*
  * Let's get ready to rumble
@@ -12,4 +15,5 @@ var $doc = $("html, body"),
 $(document).ready(function(){
   $tweets_btn.on("click", toggleTweet);
   $overlay.on("click", toggleTweet);
+  $project_grid_website.on("click", focusProjectsGrid);
 });
---
+++ dev/js/main/projects.js
@@ -0,0 +1,12 @@
+/*
+ * Project grid
+ */
+function focusProjectsGrid(e){
+  var $this = $(this),
+      $class = $this.attr("class").replace("grid-c ", "");
+
+  if($class == "project_website"){
+    //toggleWebsiteGrid
+    // Show back button as well
+  }
+}
