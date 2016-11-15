d3beb3d71d00b8c06459eae82a6c34c08f6b6daf
Ken Van Damme <solo244@gmail.com>
2016-11-15 09:19:19 +0100

    Cleaned up code a bit

--- dev/js/analytics.js
+++ dev/js/analytics.js
@@ -1,1 +1,7 @@
-(function(G,o,O,g,l){G.GoogleAnalyticsObject=O;G[O]||(G[O]=function(){(G[O].q=G[O].q||[]).push(arguments)});G[O].l=+new Date;g=o.createElement('script'),l=o.scripts[0];g.src='https://www.google-analytics.com/analytics.js';l.parentNode.insertBefore(g,l)}(this,document,'ga'));ga('create','UA-XXXX-Y');ga('send','pageview')
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-17550412-1', 'auto');
+ga('send', 'pageview');
--- dev/js/main/design.js
+++ dev/js/main/design.js
@@ -2,9 +2,8 @@
 * Load all the design images
 */
 function setDesignImages(){
-
   var amount = myDesigns.length;
-  console.log(myDesigns[amount-1].loc);
+  
   for(i = 0; i < amount; i++){
     $design_grid.prepend("<div class='grid-item'><div class='grid-c'><a href='" + myDesigns[i].loc + "', target='_blank')><i class='icon dripicons-cart'></i>Buy</a><img src='../images/designs/products/design" + (i + 1) + "." + myDesigns[i].extension + "'/></div></div>");
   }
