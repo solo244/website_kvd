f5c4166d1c016f82b4781738d60c8f98cd6f292e
Solo244 <solo244@gmail.com>
2017-11-29 20:57:49 +0100

    First step lazyload

--- dev/js/main/design.js
+++ dev/js/main/design.js
@@ -3,8 +3,10 @@
 */
 function setDesignImages(){
   var amount = myDesigns.length;
-  
+
   for(i = 0; i < amount; i++){
-    $design_grid.prepend("<div class='grid-item'><div class='grid-c'><a href='" + myDesigns[i].loc + "', target='_blank')><i class='icon dripicons-cart'></i>Buy</a><img src='../images/designs/products/design" + (i + 1) + "." + myDesigns[i].extension + "'/></div></div>");
+    $design_grid.prepend("<div class='grid-item'><div class='grid-c'><a href='" + myDesigns[i].loc + "', target='_blank')><i class='icon dripicons-cart'></i>Buy</a><img class='lazyload' src='../images/designs/products/design" + (i + 1) + "." + myDesigns[i].extension + "'/></div></div>");
   }
 }
+
+//lazyload();
