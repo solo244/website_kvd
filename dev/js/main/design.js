eb10fa6ac66f2676c5973d02836d829028fffef5
Solo244 <solo244@gmail.com>
2017-12-02 14:43:16 +0100

    Added lazyload to design images

--- dev/js/main/design.js
+++ dev/js/main/design.js
@@ -5,8 +5,11 @@ function setDesignImages(){
   var amount = myDesigns.length;
 
   for(i = 0; i < amount; i++){
-    $design_grid.prepend("<div class='grid-item'><div class='grid-c'><a href='" + myDesigns[i].loc + "', target='_blank')><i class='icon dripicons-cart'></i>Buy</a><img class='lazyload' src='../images/designs/products/design" + (i + 1) + "." + myDesigns[i].extension + "'/></div></div>");
+    $design_grid.prepend("<div class='grid-item'><div class='grid-c grid-c-design'><a href='" + myDesigns[i].loc + "', target='_blank')><i class='icon dripicons-cart'></i>Buy</a><img width='300' height='300' class='lazy' data-original='../images/designs/products/design" + (i + 1) + "." + myDesigns[i].extension + "'/></div></div>");
   }
-}
 
-//lazyload();
+  $("img.lazy").lazyload({
+    threshold : 200,
+    effect : "fadeIn"
+  });
+}
