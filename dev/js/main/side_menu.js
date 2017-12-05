775a60d32326206579f25813cba80f34e35e150b
Solo244 <solo244@gmail.com>
2017-12-05 08:03:17 +0100

    Refactor js side_menu.js

---
+++ dev/js/main/side_menu.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  // Vars
+  var $doc = $("html, body"),
+      $tutorials = $doc.find(".tutorial-content"),
+      $side_menu = $tutorials.find(".sideMenu");
+
+  // Events
+  $(window).scroll(setSideMenu);
+
+  // Add scroll top when passed 500px
+  function setSideMenu(e){
+    var scroll = $(window).scrollTop();
+
+    if(scroll > 500){
+      $side_menu.addClass("scrollTop");
+    }
+    else{
+      $side_menu.removeClass("scrollTop");
+    }
+  }
+
+})(jQuery);
