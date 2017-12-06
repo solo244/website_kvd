8bedc82e4c2c74925097bc090b439155dd211b3b
Solo244 <solo244@gmail.com>
2017-12-06 07:46:16 +0100

    Refactor js news.js

--- dev/js/main/news.js
+++ dev/js/main/news.js
@@ -1,34 +1,49 @@
-/*
-* Toggle news
-*/
-function toggleNews(e){
-  var $this = $(this);
+$(function() {
 
-  $news_items.toggleClass("news-items_active");
-}
+  // Vars
+  var $doc = $("html, body"),
+      $news = $doc.find(".news"),
+      currentNews = 1,
+      $news_next = $news.find(".next"),
+      $news_prev = $news.find(".previous"),
+      $news_items = $news.find(".news-items"),
+      $news_holder = $doc.find(".news_holder"),
+      $news_bell = $news.find(".bell");
 
-/*
-* Set news items
-*/
-function setNewsItems(){
-  var number = $news_holder.find(".grid-item").length;
+  // Events
+  $news_bell.on("click", toggleNews);
+  $news_next.find(".tri").on("click", gotoNextNews);
+  $news_next.find(".arro").on("click", gotoNextNews);
+  $news_prev.find(".tri").on("click", gotoNextPrev);
+  $news_prev.find(".arro").on("click", gotoNextPrev);
 
-  $news_holder.find("h2 span").html(number);
-}
+  // Toggle news
+  function toggleNews(e){
+    var $this = $(this);
+    $news_items.toggleClass("news-items_active");
+  }
 
-/* Slideshow navigation */
-function gotoNextNews(){
-  var number = $news_holder.find(".grid-item").length;
+  //Set news items
+  function setNewsItems(){
+    var number = $news_holder.find(".grid-item").length;
+    $news_holder.find("h2 span").html(number);
+  }
 
-  console.log(number);
-}
+  // Slideshow navigation
+  function gotoNextNews(){
+    var number = $news_holder.find(".grid-item").length;
 
-function gotoNextPrev(){
-  var number = $news_holder.find(".grid-item").length;
+    console.log(number);
+  }
 
-  console.log(number);
-}
+  function gotoNextPrev(){
+    var number = $news_holder.find(".grid-item").length;
 
-function navigateSlideshow(){
+    console.log(number);
+  }
 
-}
+  function navigateSlideshow(){
+
+  }
+
+})(jQuery);
