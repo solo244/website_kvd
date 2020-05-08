949af27308f3e7b36dd76d64d95f67b622a3aaf9
Solo244 <solo244@gmail.com>
2020-05-08 13:14:00 +0200

    Add categories for homepage

---
+++ src/components/categories/index.svelte
@@ -0,0 +1,36 @@
+<!-- Styles -->
+<style lang="scss">
+@import "./categories.scss";
+</style>
+
+<!-- Scripts -->
+<script>
+</script>
+
+<!-- HTML -->
+<section class="categories">
+  <a href="/code" class="code">
+    <div class="bg"></div>
+    <div class="button">Take a look</div>
+    <h2>Code</h2>
+    <p>Sure, I make websites. But I'm also a procrastinator Exctraordinaire. This results in many side projects.</p>
+  </a>
+  <a href="/tutorials" class="tutorials">
+    <div class="bg"></div>
+    <div class="button">Read</div>
+    <h2>Tutorials</h2>
+    <p>I like to share my web design & development knowledge in "short-ish" written tutorials.</p>
+  </a>
+  <a href="/creative" class="creative">
+    <div class="bg"></div>
+    <div class="button">Explore</div>
+    <h2>Creative</h2>
+    <p>I got tired of looking for clothes that have awesome designs. So I make them.</p>
+  </a>
+  <a href="/dnd" class="dnd">
+    <div class="bg"></div>
+    <div class="button">Check it</div>
+    <h2>D&D</h2>
+    <p>I love Dungeons and Dragons and I'm a front-ender. So I combine them were possible.</p>
+  </a>
+</section>
