208bf305fda7ffc8a340c54ad69d91e757a4bef2
Solo244 <solo244@gmail.com>
2020-05-10 16:05:59 +0200

    Add base footer

--- src/components/footer/index.svelte
+++ src/components/footer/index.svelte
@@ -11,35 +11,17 @@ import Logo from "../../images/logo-42.svg";
 <!-- HTML -->
 <div class="footer-container">
   <nav class="footer">
-    <div class="col-alt">
-      <a href="/" class="logo">
-        {@html Logo}
-      </a>
-      <p>Webdesigner, webdeveloper, teacher, gamer, moron, realist, procrastinator, fast talker, nerd, distracted by pretty colors. </p>
-      <p>© 2020 Ken Van Damme, No-Inc.</p>
-    </div>
-    <ul class="col">
-      <li><strong><a class="" href="/">General</a></strong></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
+    <a href="/" class="logo">
+      {@html Logo}
+    </a>
+    <ul>
+      <li><a class="" href="/">Code</a></li>
+      <li><a class="" href="/">Tutorials</a></li>
+      <li><a class="" href="/">Creative</a></li>
+      <li><a class="" href="/">D&D</a></li>
     </ul>
-
-    <ul class="col">
-      <li><strong><a class="" href="/">Me</a></strong></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-    </ul>
-
-    <ul class="col">
-      <li><strong><a class="" href="/">Updates</a></strong></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
-      <li><a class="" href="/">#1</a></li>
+    <ul class="socials">
+      <li><a href="#">Twitter</a></li>
     </ul>
   </nav>
-</div> 
+</div>
