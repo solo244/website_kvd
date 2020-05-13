1c0dd31de6564e93cfa967f6eb59bd1976db1817
Solo244 <solo244@gmail.com>
2020-05-13 17:18:00 +0200

    Added design and code constants

---
+++ src/constants/code.js
@@ -0,0 +1,64 @@
+const code = [
+
+  {
+    id: "stalstudio",
+    title: "Stalstudio",
+    link: "https://www.stalstudio.be",
+    type: "website",
+    status: true,
+    description: "Stallers are people with a passion for theatre."
+  },
+  {
+    id: "itsunited",
+    title: "It's United",
+    link: "https://www.its-united.com/",
+    type: "website",
+    status: true,
+    description: "ITS-United creates sustainable traffic management infrastructures."
+  },
+  {
+    id: "criticalchronicles",
+    title: "The Critical Chronicles",
+    link: "https://www.criticalchronicles.com",
+    type: "website",
+    status: true,
+    description: "Follow our two D&D squads, destined to .... the world."
+  },
+
+  {
+    id: "burntabs",
+    title: "Burntabs",
+    link: "https://burntabs.kenvandamme.be/",
+    type: "app",
+    status: true,
+    description: "The biggest tab burner extension for Chrome and Firefox."
+  },
+  {
+    id: "mine",
+    title: "Mine",
+    link: "",
+    type: "app",
+    status: false,
+    description: "Manage your browser favorites, your way."
+  },
+  {
+    id: "dnd",
+    title: "D&D",
+    link: "",
+    type: "app",
+    status: false,
+    description: "."
+  },
+
+  {
+    id: "dacss",
+    title: "DACSS",
+    link: "",
+    type: "github",
+    status: false,
+    description: "Own framework."
+  }
+
+];
+
+export default code;
