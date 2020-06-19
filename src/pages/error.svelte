670b1011bc78e70f9422cf28e1d9f3e226cbddcd
Solo244 <solo244@gmail.com>
2020-06-19 23:53:22 +0200

    Small folder cleanup

--- src/pages/error.svelte
+++ src/pages/error.svelte
@@ -1,19 +1,21 @@
 <!-- Styles -->
 <style lang="scss">
+@import "../../styles/utility";
 </style>
 
 <!-- Scripts -->
 <script>
+import Button from "../components/button/index.svelte";
 </script>
 
 <!-- HTML -->
-<section style="text-align: center">
+<section class="center">
 	<h1>What just happened?!</h1>
-	<img src="/images/page-not-found.png" alt="Page not found" width=300>
+	<img src="/images/page-not-found.png" alt="Page not found" width="300">
 	<h4>This page doesn't exist</h4>
 	<p>Which is a seemingly absurd and contradictory statement when thinking of the fact that you are on this page now..</p>
 	<div class="btn-group">
-		<button>Go back home</button>
-		<button>Down the rabbit hole</button>
+		<Button label="Go back home" />
+		<Button label="Down the rabbit hole" />
 	</div>
 </section>
--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,10 +1,11 @@
 <!-- Styles -->
 <style lang="scss">
+@import "../../styles/layout";
 </style>
 
 <!-- Scripts -->
 <script>
-import Categories from "../components/categories/index.svelte";
+import Button from "../components/button/index.svelte";
 </script>
 
 <!-- HTML -->
@@ -14,7 +15,32 @@ import Categories from "../components/categories/index.svelte";
   As a profession, I make web-stuff*.</h3>
 </header>
 
-<Categories />
+<section class="container--4">
+  <a href="/coding" class="block--home coding">
+    <div class="bg"></div>
+    <Button label="Take a look"/>
+    <h2>Coding</h2>
+    <p>Sure, I make websites. But I'm also a procrastinator Exctraordinaire. This results in many side projects.</p>
+  </a>
+  <a href="/writing" class="block--home writing">
+    <div class="bg"></div>
+    <Button label="Read"/>
+    <h2>Writing</h2>
+    <p>I like to share my web design & development knowledge in "short-ish" written tutorials.</p>
+  </a>
+  <a href="/designing" class="block--home designing">
+    <div class="bg"></div>
+    <Button label="Explore"/>
+    <h2>Designing</h2>
+    <p>I got tired of looking for clothes that have awesome designs. So I make them.</p>
+  </a>
+  <a href="/dnding" class="block--home dnding">
+    <div class="bg"></div>
+    <Button label="Check it"/>
+    <h2>D&Ding</h2>
+    <p>I love Dungeons and Dragons and I'm a front-ender. So I combine them were possible.</p>
+  </a>
+</section>
 
 <section>
   <h3>Webdesigner, webdeveloper, teacher, gamer, moron, realist, procrastinator, fast talker, nerd, distracted by pretty colors. I think thats by far the best description I can give you. I am a man of few words but I can use some visuals to help me tell you more about myself. Keep scrolling.</h3>
