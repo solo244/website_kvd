3ba319bd907dea9e9ff377d6e90e26fb06b9262b
Solo244 <solo244@gmail.com>
2020-06-19 23:47:56 +0200

    Renaming pages

---
+++ src/pages/writing.svelte
@@ -0,0 +1,47 @@
+<!-- Styles -->
+<style lang="scss">
+@import "./pages.scss";
+</style>
+
+<!-- Scripts -->
+<script>
+</script>
+
+<!-- HTML -->
+<section>
+	<h1>We can't help everyone, but everyone can help someone.</h1>
+</section>
+
+<section class="blog-columns">
+  <article href="/" class="">
+    <a href="/" class="btn">View all</a>
+    <h2>WDIGT*</h2>
+    <ul>
+      <li>
+        <a href="/">
+          <span>CSS selector starts with</span>
+        </a>
+      </li>
+    </ul>
+  </article>
+	<article href="/" class="">
+    <a href="/">View all</a>
+    <h2>Blog posts</h2>
+    <ul>
+      <li>
+        <a href="/">
+        </a>
+      </li>
+    </ul>
+  </article>
+	<article href="/" class="">
+    <a href="/">View all</a>
+    <h2>Tutorials</h2>
+    <ul>
+      <li>
+        <a href="/">
+        </a>
+      </li>
+    </ul>
+  </article>
+</section>
