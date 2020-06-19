180e663eb57f981db776352f4c864ddc5b7737f1
Solo244 <solo244@gmail.com>
2020-06-19 23:52:33 +0200

    WIP: button component

---
+++ src/components/button/index.svelte
@@ -0,0 +1,16 @@
+<!-- Styles -->
+<style lang="scss">
+@import "./buttons.scss";
+</style>
+
+<!-- Scripts -->
+<script>
+</script>
+
+<!-- HTML -->
+<a href="/" class="button">
+
+</a>
+<button class="button">
+
+</button>
