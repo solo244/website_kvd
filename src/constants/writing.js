3363bb0b7c0e99076855df4541410c12ad7640d7
Solo244 <solo244@gmail.com>
2020-06-24 22:55:22 +0200

    Added writing constans

---
+++ src/constants/writing.js
@@ -0,0 +1,64 @@
+const writing = {
+
+  tutorials: [
+    {
+      title: "Floats & clears #1",
+      icon: "css",
+      category: "css",
+      link: "/float",
+    },
+    {
+      title: "Floats & clears #2",
+      icon: "css",
+      category: "css",
+      link: "/float_clear",
+    },
+    {
+      title: "Positioning #1",
+      icon: "css",
+      category: "css",
+      link: "/positioning",
+    },
+    {
+      title: "Fullscreen background",
+      icon: "css",
+      category: "css",
+      link: "/fullscreen",
+    },
+    {
+      title: "CSS filters",
+      icon: "css",
+      category: "css",
+      link: "/filters",
+    },
+    {
+      title: "Making a menu",
+      icon: "css",
+      category: "css",
+      link: "/menu",
+    },
+    {
+      title: "CSS 3",
+      icon: "css",
+      category: "css",
+      link: "/css3",
+    },
+    {
+      title: "Responsive webdesign",
+      icon: "css",
+      category: "css",
+      link: "/responsive",
+    }
+  ],
+
+  // blog: {
+
+  // },
+
+  // wdigt: {
+
+  // }
+
+};
+
+export default writing;
