9539a9589a5404d5acc195f3f57231d1de8c2e03
Solo244 <solo244@gmail.com>
2020-06-24 22:55:35 +0200

    Error page content

--- src/pages/error.svelte
+++ src/pages/error.svelte
@@ -14,8 +14,5 @@ import Button from "../components/button/index.svelte";
 	<img src="/images/page-not-found.png" alt="Page not found" width="300">
 	<h4>This page doesn't exist</h4>
 	<p>Which is a seemingly absurd and contradictory statement when thinking of the fact that you are on this page now..</p>
-	<div class="btn-group">
-		<Button label="Go back home" />
-		<Button label="Down the rabbit hole" />
-	</div>
+	<Button label={["Go back home", "Down the rabbit hole"]} href={["/", "/"]} />
 </section>
