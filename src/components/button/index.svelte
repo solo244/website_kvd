edc9b79d286f000c0e7600143845f2085a72db58
Solo244 <solo244@gmail.com>
2020-08-04 00:35:18 +0200

    Added button component

--- src/components/button/index.svelte
+++ src/components/button/index.svelte
@@ -5,33 +5,36 @@
 
 <!-- Scripts -->
 <script>
-export let label = "";
-export let href = "";
+  export let label = "";
+  export let href = "";
+  export let type = "";
 </script>
 
 <!-- HTML -->
-{#if typeof label === "object"}
-  <div class="button-group">
-    {#each label as item, i}
-      {#if typeof href === "object"}
-        <a href="/" class="button">
-          {item}
-        </a>
-      {:else}
-        <button class="button">
-          {item}
-        </button>
-      {/if}
-    {/each}
-  </div>
-{:else}
-  {#if href !== ""}
-    <a href="/" class="button">
-      {label}
-    </a>
+{#if label !== ''}
+  {#if typeof label === "object"}
+    <div class="button-group">
+      {#each label as item, i}
+        {#if typeof href === "object"}
+          <a href="/" class="button">
+            {item}
+          </a>
+        {:else}
+          <button class="button">
+            {item}
+          </button>
+        {/if}
+      {/each}
+    </div>
   {:else}
-    <button class="button">
-      {label}
-    </button>
+    {#if href !== ""}
+      <a href={href} class={`button ${type}`}>
+        {label}
+      </a>
+    {:else}
+      <button class={`button ${type}`}>
+        {label}
+      </button>
+    {/if}
   {/if}
 {/if}
