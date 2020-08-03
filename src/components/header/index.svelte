e972787d11bc5555aaf03cdfbbcc8f794c9e9432
Solo244 <solo244@gmail.com>
2020-08-04 00:36:17 +0200

    Added header component

--- src/components/header/index.svelte
+++ src/components/header/index.svelte
@@ -1,52 +1,21 @@
-<!-- Styles -->
 <style lang="scss">
-@import "header.scss";
+@import "./header.scss";
 </style>
 
-<!-- Scripts -->
 <script>
-import Logo from "../../images/logo-42.svg";
-const domain = "https://www.kenvandamme.be";
-const testdomain = "http://localhost:5000"
-let active = "";
+  import Button from "../button/index.svelte";
 
-const setActive = () => {
-  setInterval(function() {
-    const loc = window.location.href;
-
-    if (loc.endsWith("5000/") || loc.endsWith("kenvandamme.be/") || loc.endsWith("kenvandamme.be")) {
-      active = "home";
-    } else if (loc.startsWith(testdomain + "/coding") || loc.startsWith(domain + "/coding")) {
-      active="coding";
-    } else if (loc.startsWith(testdomain + "/writing") || loc.startsWith(domain + "/writing")) {
-      active="writing";
-    } else if (loc.startsWith(testdomain + "/designing") || loc.startsWith(domain + "/designing")) {
-      active="designing";
-    } else if (loc.startsWith(testdomain + "/dnding") || loc.startsWith(domain + "/dnding")) {
-      active="dnding";
-    }
-    else {
-      active= "";
-    }
-  }, 100);
-}
-
-setActive();
+  export let title;
+  export let content;
+  export let button;
+  export let link;
+  export let background;
 </script>
 
-<!-- HTML -->
-<nav class="container--header">
-  <div class="breadcrumbs">
-    <a href="/" class="logo">
-      {@html Logo}
-    </a>
-    <span>?</span>
-  </div>
-  <ul class="menu">
-    <li><a class:active={active === "home"} class="link-home" href="/" on:click={() => setActive()}>Home</a></li>
-    <li><a class:active={active === "coding"} class="link-code" href="/coding" on:click={() => setActive()}>Coding</a></li>
-    <li><a class:active={active === "writing"} class="link-tutorials" href="/writing" on:click={() => setActive()}>Writing</a></li>
-    <li><a class:active={active === "designing"} class="link-creative" href="/designing" on:click={() => setActive()}>Designing</a></li>
-    <li><a class:active={active === "dnding"} class="link-dnd" href="/dnding" on:click={() => setActive()}>D&Ding</a></li>
-  </ul>
-</nav>
+<header class={`header ${background !== undefined ? background : ''}`}>
+  <section class="container">
+    <h1>{title}</h1>
+    <p>{content}</p>
+    <Button label={button} href={link} type={"primary"} />
+  </section>
+</header>
