7972958c27220949e7746e1e256e18b1ac226268
Solo244 <solo244@gmail.com>
2020-08-04 00:29:50 +0200

    Final config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
-import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
 import autoPreprocess from "svelte-preprocess";
-import svg from "rollup-plugin-svg-import";
+import { terser } from "rollup-plugin-terser";
+import config from "sapper/config/rollup.js";
+import pkg from "./package.json";
 
-const production = !process.env.ROLLUP_WATCH;
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) => (warning.code === "CIRCULAR_DEPENDENCY" && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 
 const preprocess = autoPreprocess({
   scss: {
@@ -17,59 +23,97 @@ const preprocess = autoPreprocess({
   },
 });
 
-const MAIN = {
-	input: "src/main.js",
-	output: {
-		sourcemap: true,
-		format: "iife",
-		name: "app",
-		file: "public/build/main.js"
+export default {
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
+			replace({
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			svelte({
+				dev,
+				preprocess,
+				hydratable: true,
+				emitCss: true
+			}),
+			resolve({
+				browser: true,
+				dedupe: ["svelte"]
+			}),
+			commonjs(),
+
+			legacy && babel({
+				extensions: [".js", ".mjs", ".html", ".svelte"],
+				babelHelpers: "runtime",
+				exclude: ["node_modules/@babel/**"],
+				presets: [
+					["@babel/preset-env", {
+						targets: "> 0.25%, not dead"
+					}]
+				],
+				plugins: [
+					"@babel/plugin-syntax-dynamic-import",
+					["@babel/plugin-transform-runtime", {
+						useESModules: true
+					}]
+				]
+			}),
+
+			!dev && terser({
+				module: true
+			})
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			replace({
+				"process.browser": false,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			svelte({
+				generate: "ssr",
+				dev
+			}),
+			resolve({
+				dedupe: ["svelte"]
+			}),
+			commonjs()
+		],
+		external: Object.keys(pkg.dependencies).concat(require("module").builtinModules),
+
+		preserveEntrySignatures: "strict",
+		onwarn,
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			commonjs(),
+			!dev && terser()
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
 	}
 };
 
-const COMMON = {
-	plugins: [
-		svg({ stringify: true }),
-		svelte({
-			dev: !production,
-			preprocess,
-			css: css => {
-				css.write("public/build/bundle.css");
-			}
-		}),
 
-		resolve({
-			browser: true,
-			dedupe: importee => importee === "svelte" || importee.startsWith("svelte/")
-		}),
-		commonjs(),
 
-		!production && serve(),
-		!production && livereload("public"),
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
-};
 
-export default [
-	Object.assign({}, MAIN, COMMON),
-];
 
-function serve() {
-	let started = false;
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
 
-				require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-					stdio: ["ignore", "inherit", "inherit"],
-					shell: true
-				});
-			}
-		}
-	};
-}
