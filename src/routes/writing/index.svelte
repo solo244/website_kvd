4684b80d93e0c016d23f9706260f5c1b6826ab57
Solo244 <solo244@gmail.com>
2020-08-06 23:22:15 +0200

    First attempt at writing folder - content type

--- src/routes/writing/index.svelte
+++ src/routes/writing/index.svelte
@@ -1,12 +1,29 @@
 <style>
 </style>
 
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`writing.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
 <script>
 	import Header from "../../components/header/index.svelte";
+	export let posts;
 </script>
 
 <svelte:head>
 	<title>Writing | Ken Van Damme</title>
 </svelte:head>
 
-<Header title="Writing" content="This is the 'about' page. There's not much here." />
+<Header
+	title="Writing"
+	content="We can't help everyone, but everyone can help someone."
+	background="writing"
+/>
+
+{#each posts as post}
+	<a rel="prefetch" href="writing/{post.slug}">{post.title}</a>
+{/each}
