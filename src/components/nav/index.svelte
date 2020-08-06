92e659c8583932c519a01c0b68dd611e8b08ac80
Solo244 <solo244@gmail.com>
2020-08-06 23:24:00 +0200

    Added social icons and links

--- src/components/nav/index.svelte
+++ src/components/nav/index.svelte
@@ -3,18 +3,44 @@
 </style>
 
 <script>
+  // import { twitter } from "jam-icons";
+  import {
+    TwitterIcon,
+    GithubIcon,
+    LinkedinIcon,
+    MailIcon
+  } from "svelte-feather-icons";
 	export let segment;
 </script>
 
 <nav class="nav">
   <a href="." class="nav__logo">KeVD_</a>
   <ul class="nav__menu">
-    <li><a rel=prefetch class:active="{segment === 'writing'}" class="link-writing" href="/writing">Writing</a></li>
-    <li><a rel=prefetch class:active="{segment === 'coding'}" class="link-coding" href="/coding">Coding</a></li>
-    <li><a rel=prefetch class:active="{segment === 'designing'}" class="link-designing" href="/designing">Designing</a></li>
-    <li><a rel=prefetch class:active="{segment === 'gaming'}" class="link-gaming" href="/gaming">Gaming</a></li>
+    <li><a rel=prefetch class:active="{segment === "writing"}" class="link-writing" href="/writing">Writing</a></li>
+    <li><a rel=prefetch class:active="{segment === "coding"}" class="link-coding" href="/coding">Coding</a></li>
+    <li><a rel=prefetch class:active="{segment === "designing"}" class="link-designing" href="/designing">Designing</a></li>
+    <li><a rel=prefetch class:active="{segment === "gaming"}" class="link-gaming" href="/gaming">Gaming</a></li>
   </ul>
   <ul class="nav__socials">
-    <li><a href="https://twitter.com/Elsolo244" target="_blank">Twitter</a></li>
+    <li>
+      <a rel="me" href="https://twitter.com/Elsolo244" target="_blank">
+        <TwitterIcon size="20" />
+      </a>
+    </li>
+    <li>
+      <a rel="me" href="https://github.com/solo244" target="_blank">
+        <GithubIcon size="20" />
+      </a>
+    </li>
+    <li>
+      <a rel="me" href="https://www.linkedin.com/in/ken-van-damme" target="_blank">
+        <LinkedinIcon size="20" />
+      </a>
+    </li>
+    <li>
+      <a href="mailto:info.kenvandamme.be" target="_blank">
+        <MailIcon size="20" />
+      </a>
+    </li>
   </ul>
 </nav>
