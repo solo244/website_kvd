6f2d6c8e2f7d56265449c38fe32a1ebea6d64508
Solo244 <solo244@gmail.com>
2020-08-07 23:38:57 +0200

    Back to top function

--- src/components/footer/index.svelte
+++ src/components/footer/index.svelte
@@ -2,12 +2,26 @@
 @import "footer.scss";
 </style>
 
+<script>
+  import {
+    ArrowUpIcon
+  } from "svelte-feather-icons";
+
+  function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+</script>
+
 <nav class="footer">
   <section class="container">
     <article>
       Icons from <a href="https://icons8.com/" target="_blank">Icon8</a>
     </article>
     <!-- Back to top -->
+    <div class="to-top" on:click={() => toTop()}>
+      <ArrowUpIcon size="20" />
+    </div>
     <aside>
       <span>© 2020</span> Ken Van Damme
     </aside>
