8aeea82059980c3b2a6d222bfe353c3af7379d99
Solo244 <solo244@gmail.com>
2020-08-26 09:42:42 +0200

    Filter component

---
+++ src/components/filter/index.svelte
@@ -0,0 +1,36 @@
+<style lang="scss">
+@import "./filter.scss";
+</style>
+
+<script>
+// let show = ;
+// let as = ;
+// let filter = ;
+</script>
+
+<div class="container">
+  <aside class="filter">
+    <section class="filter__block">
+      <ul class="filter__main">
+        <li>SHOW</li>
+        <li>All</li>
+        <li>What Did I Google Today</li>
+        <li>Brainfarts</li>
+        <li>Tutorials</li>
+      </ul>
+      <ul class="filter__main">
+        <li>AS</li>
+        <li>Grid</li>
+        <li>List</li>
+      </ul>
+    </section>
+    <section class="filter__block">
+      <ul class="filter__sub">
+        <li>FILTERS</li>
+        <li>ALL</li>
+        <li>CSS</li>
+        <li>JS</li>
+      </ul>
+    </section>
+  </aside>
+</div>
