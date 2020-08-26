c04e9de19ee1b8f63e387c15bd01290ace3741b5
Solo244 <solo244@gmail.com>
2020-08-26 09:42:14 +0200

    Homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,30 @@
-<style lang="scss">
+<style lang="scss" global>
 @import "../styles/pages";
 @import "../styles/filters";
 </style>
 
-<script context="module">
+<!-- <script context="module">
 	export function preload({ params, query }) {
-		return this.fetch(`writing.json`).then(r => r.json()).then(writings => {
+		const writings = this.fetch(`writing.json`).then(r => r.json()).then(writings => {
 			return { writings };
 		});
+		const codings = this.fetch(`coding.json`).then(r => r.json()).then(codings => {
+			return { codings };
+		});
+
+		return codings, writings;
 	}
-</script>
+</script> -->
 
 <script>
 	import { onMount } from "svelte";
-	import writing from "../constants/writing.js";
 	import Header from "../components/header/index.svelte";
 	import Button from "../components/button/index.svelte";
 
 	export let writings;
+	export let codings;
 
-	let writingMode = false;
+	let toggles = [""];
 
 	onMount(() => {
     if (window.netlifyIdentity) {
@@ -33,9 +38,13 @@
     }
   });
 
-	function toggleMode(type, mode) {
-		if (type === "writing") {
-			writingMode = !mode;
+	function toggleMode(type) {
+		if (toggles.includes(type)) {
+			toggles = toggles.filter(toggle => toggle !== type);
+		} else {
+			let added = toggles;
+			added.push(type);
+			toggles = added;
 		}
 	}
 </script>
@@ -53,9 +62,20 @@
 	link="/about"
 />
 
-<section class="container grid">
+<section class="container grid--stacked">
 	<section class="grid__row">
-		<article class="grid__item" class:active={writingMode}>
+		{#if writings !== undefined && writings !== null}
+			{#each writings as writing, i}
+				{writing.title}
+			{/each}
+		{/if}
+
+		{#if codings !== undefined && codings !== null}
+			{#each codings as coding, i}
+				{coding.title}
+			{/each}
+		{/if}
+		<!-- <article class="grid__item" class:active={toggles.includes("writing")}>
 			<h2>Writing</h2>
 			<ul>
 				{#each writings as writing, i}
@@ -74,12 +94,34 @@
 				{/each}
 			</ul>
 			<div class="grid__buttons">
-				<button on:click={() => toggleMode("writing", writingMode)} type="primary">Open</button>
-				<a href="/writing" class="button primary">All</a>
+				<Button on:click={() => toggleMode("writing")} type="primary" label="Open" icon="plus" group />
+				<Button href="/writing" label="All" icon="right" position="right" group />
 			</div>
-		</article>
+		</article> -->
 
-		<article></article>
+		<!-- <article class="grid__item" class:active={toggles.includes("coding")}>
+			<h2>Coding</h2>
+			<ul>
+				{#each codings as coding, i}
+					{#if i < 3}
+						<li>
+							<a href="{coding.link}" class="grid__item__link coding">
+								<span>{coding.description}</span>
+								<div class={`postfilter grid__item__category ${coding.category}`}>{coding.category}</div>
+								<h3>{coding.title}</h3>
+							</a>
+						</li>
+					{/if}
+				{:else}
+					<li>No projects found</li>
+				{/each}
+			</ul>
+			<div class="grid__buttons">
+				<Button on:click={() => toggleMode("coding")} type="primary" label="Open" icon="plus" group />
+				<Button href="/coding" label="All" icon="right" position="right" group />
+			</div>
+		</article> -->
+
 		<article></article>
 		<article></article>
 	</section>
