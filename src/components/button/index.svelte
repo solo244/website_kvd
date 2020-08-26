c17d570f56ce9520afc457b286c508348d9293a3
Solo244 <solo244@gmail.com>
2020-08-26 09:42:30 +0200

    Button component

--- src/components/button/index.svelte
+++ src/components/button/index.svelte
@@ -5,36 +5,31 @@
 
 <!-- Scripts -->
 <script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  import Content from "./_content.svelte";
+
   export let label = "";
   export let href = "";
   export let type = "";
+  export let icon = "";
+  export let position = "";
+  export let group;
+
+  function click() {
+    dispatch("click");
+  }
 </script>
 
 <!-- HTML -->
-{#if label !== ''}
-  {#if typeof label === "object"}
-    <div class="button-group">
-      {#each label as item, i}
-        {#if typeof href === "object"}
-          <a href="/" class="button">
-            {item}
-          </a>
-        {:else}
-          <button class="button">
-            {item}
-          </button>
-        {/if}
-      {/each}
-    </div>
+{#if label !== ""}
+  {#if href !== ""}
+    <a href={href} class={`button ${type}`} class:grouped={group}>
+      <Content icon={icon} position={position} item={label} group={group} />
+    </a>
   {:else}
-    {#if href !== ""}
-      <a href={href} class={`button ${type}`}>
-        {label}
-      </a>
-    {:else}
-      <button class={`button ${type}`}>
-        {label}
-      </button>
-    {/if}
+    <button class={`button ${type}`} on:click={click} class:grouped={group}>
+      <Content icon={icon} position={position} item={label} group={group} />
+    </button>
   {/if}
 {/if}
