cae8c1ae0dd9780a36100dc3f606622122271e04
Solo244 <solo244@gmail.com>
2020-09-02 21:18:48 +0200

    WIP: added first utils yay

--- src/constants/utils.js
+++ src/constants/utils.js
@@ -1,6 +1,21 @@
 // import moment from "../../node_modules/moment";
+// import moment from "moment";
 
 const util = {
+
+  convertToDate: function(date) {
+    console.log(date);
+  },
+
+  sortByDate: function(sortby) {
+    console.log(sortby);
+    // var byDate = arrayOfObjects.slice(0);
+    // byDate.sort(function(a,b) {
+    //   return a.born - b.born;
+    // });
+    // console.log('by date:');
+    // console.log(byDate);
+  }
 
 };
 
