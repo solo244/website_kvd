11a6e2d9c7e696be4383cfe592612994ef4dcdc1
Solo244 <solo244@gmail.com>
2020-09-03 08:39:30 +0200

    Main layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,13 +1,15 @@
 <style lang="scss" global>
 @import "../styles/reset";
 @import "../styles/normalise";
+@import "../styles/pages";
+@import "../styles/filters";
 </style>
 
 <script>
 	import Nav from '../components/nav/index.svelte';
 	import Footer from '../components/footer/index.svelte';
 
-	export let segment;
+	export let segment = "";
 </script>
 
 <Nav {segment}/>
