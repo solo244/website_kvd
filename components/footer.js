d35c74373641bbee40855c5a7aa93f3f7e27b2c2
Solo244 <solo244@gmail.com>
2020-10-20 23:45:24 +0200

    Footer component

---
+++ components/footer.js
@@ -0,0 +1,52 @@
+import styles from "../styles/modules/footer.module.css";
+import { ArrowUp, GitHub, Twitter } from "react-feather";
+
+const toTop = () =>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+let year = new Date().getFullYear();
+
+export default function Footer({ children, grid }) {
+  return (
+    <>
+      {grid ? (
+        <footer className={styles.footerstripped}>
+          <aside className={styles.socials}>
+            <GitHub />
+            <Twitter />
+          </aside>
+
+          <section className={styles.disclaimer}>
+            <section className="copyright"><span>© {year}</span> Ken Van Damme</section>
+            <section className="adds">No <span>🍪 cookies, 👣 tracking, 💰 ads or ❗pop-ups.</span></section>
+          </section>
+        </footer>
+      ) : (
+        <footer className={styles.footer}>
+          <section className={styles.container}>
+            <div className={styles.totop} onClick={() => toTop()}>
+              <ArrowUp size="20" />
+            </div>
+
+            <aside className={styles.socials}>
+              <GitHub />
+              <Twitter />
+            </aside>
+
+            <section className={styles.disclaimer}>
+              <article className="copyright">
+                <span>© {year}</span> Ken Van Damme
+              </article>
+              <article className="adds">
+                No <span>🍪 cookies, 👣 tracking, 💰 ads or ❗pop-ups.</span>
+              </article>
+              {/* // BUT I STILL WNAT A COOKIE NOTIFICATION */}
+            </section>
+          </section>
+        </footer>
+      )}
+    </>
+  )
+}
