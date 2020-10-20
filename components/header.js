8ab0a1766e8a908eaffabcbf243fbe3c752a5bef
Solo244 <solo244@gmail.com>
2020-10-20 23:45:33 +0200

    Add container to header

--- components/header.js
+++ components/header.js
@@ -1,9 +1,11 @@
 import styles from "../styles/modules/header.module.css";
 
-export default function Header({ children }) {
+export default function Header({ children, style }) {
   return (
-    <header className={styles.header}>
-      {children}
+    <header className={`${styles.header} ${styles[style]}`}>
+      <section className={styles.container}>
+        {children}
+      </section>
     </header>
   )
 }
