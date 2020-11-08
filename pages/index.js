ccbcd8eae60307fdd0941c7f88fcd84b87773f25
Solo244 <solo244@gmail.com>
2020-11-08 23:29:48 +0100

    Add date constraint in there

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import Footer from "../components/footer";
 import tileStyles from "../styles/modules/tile.module.css";
 import categoryStyles from "../styles/modules/category.module.css";
 import { getSortedWritingsData } from "../lib/writings";
+import { getSortedDesignsData } from "../lib/designs";
+import { getSortedProjectsData } from "../lib/projects";
 import { Calendar } from "react-feather";
 
 export default function Home({ data }) {
@@ -38,33 +40,59 @@ export default function Home({ data }) {
       </Sidebar>
 
       <Content grid>
-        {data.map(({ slug, title, collection, date, category }) => {
-          return (
-            <article key={slug} className={tileStyles.tile}>
-              <Link href={`/writings/${slug}`}>
-                <a
-                  className={`${tileStyles.tilelink} ${categoryStyles[category]}`}
-                >
-                  <h4>{collection}</h4>
-                  <span className={tileStyles.tiledate}>
-                    <Calendar size="16" /> {date}
-                  </span>
-                  <h2>{title}</h2>
-                </a>
-              </Link>
-            </article>
-          );
-        })}
+        {data.map(
+          ({ slug, blank, title, category, filters, date, background }) => {
+            return (
+              <article key={slug} className={tileStyles.tile}>
+                <Link href={`${slug}`}>
+                  <a
+                    className={`${tileStyles.tilelink} ${categoryStyles[category]}`}
+                    target={`${blank ? "_blank" : "_self"}`}
+                  >
+                    {background ? (
+                      <aside className={tileStyles.tileimage}>
+                        <img src={`images/designs/${background}`} alt={title} />
+                      </aside>
+                    ) : (
+                      <h4>
+                        {category}
+                        {filters && `: ${filters}`}
+                      </h4>
+                    )}
+                    <span className={tileStyles.tiledate}>
+                      <Calendar size="16" /> {date}
+                    </span>
+                    <h2>{title}</h2>
+                  </a>
+                </Link>
+              </article>
+            );
+          }
+        )}
       </Content>
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const data = getSortedWritingsData();
+  let data = [];
+  const writings = getSortedWritingsData();
+  const designs = getSortedDesignsData();
+  const projects = getSortedProjectsData();
+  writings.map(writing => data.push(writing));
+  designs.map(design => data.push(design));
+  projects.map(project => data.push(project));
+
+  function dateToNum(d) {
+    d = d.split("/");
+    return Number(d[2] + d[1] + d[0]);
+  }
+
+  data.sort(function (a, b) {
+    return dateToNum(b.date) - dateToNum(a.date);
+  });
+
   return {
-    props: {
-      data,
-    },
+    props: { data: data },
   };
 }
