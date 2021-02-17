a6a4b1d7d8dcdb575141b6558fadadeae4606ddb
Solo244 <solo244@gmail.com>
2021-02-17 14:45:15 +0100

    Add categories and links constants

---
+++ constants/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    label: "All",
+    link: "all",
+    type: "secondary",
+  },
+  {
+    label: "What Did I Google Today",
+    link: "wdigt",
+    type: "warning",
+  },
+  {
+    label: "Brainfarts",
+    link: "brainfarts",
+    type: "success",
+  },
+  {
+    label: "Tutorials",
+    link: "tutorials",
+    type: "error",
+  },
+];
+
+export default categories;
---
+++ constants/links.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    path: "/about",
+    label: "About",
+  },
+  {
+    path: "/writing",
+    label: "Writing",
+  },
+  {
+    path: "/coding",
+    label: "Coding",
+  },
+  {
+    path: "/designing",
+    label: "Designing",
+  },
+];
+
+export default links;
