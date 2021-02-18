582addf1421b4140a0a06c1a6f7747f7c49571db
Solo244 <solo244@gmail.com>
2021-02-18 10:04:06 +0100

    Main app loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import "../styles/reset.css";
-import "../styles/vars.css";
-import "../styles/normalise.css";
+// Dependencies
+import { useState, useEffect } from "react";
+// Styles
+import { CssBaseline, GeistProvider } from "@geist-ui/react";
+import Sun from "@geist-ui/react-icons/sun";
+import Moon from "@geist-ui/react-icons/moon";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }) => {
+  const [themeType, setThemeType] = useState("dark");
+
+  const switchThemes = () => {
+    const update = themeType === "dark" ? "light" : "dark";
+    window.localStorage.setItem("themepref", update);
+    setThemeType(update);
+  };
+
+  useEffect(() => {
+    const theme = window.localStorage.getItem("themepref");
+    setThemeType(theme);
+  });
+
+  return (
+    <GeistProvider themeType={themeType}>
+      <CssBaseline />
+      <div
+        style={{ position: "absolute", top: "20px", right: "20px" }}
+        onClick={() => switchThemes()}
+      >
+        {themeType === "dark" ? <Sun /> : <Moon />}
+      </div>
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
+};
+
+export default App;
