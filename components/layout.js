ace5bf045ed28dc3f7c81ae5cc646ca0aebcabc9
Solo244 <solo244@gmail.com>
2021-02-18 10:03:29 +0100

    Default layout

--- components/layout.js
+++ components/layout.js
@@ -1,50 +1,25 @@
+// Dependencies
+import React from "react";
 import Head from "next/head";
-import styles from "../styles/modules/layout.module.css";
+// Components
+import Meta from "./meta";
+import Header from "./header";
+import Footer from "./footer";
+// Styles
+import { Page } from "@geist-ui/react";
 
-export default function Layout({ children }) {
+const Layout = ({ title, children }) => {
   return (
-    <>
+    <Page>
       <Head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <meta
-          name="viewport"
-          content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1"
-        />
-        <meta name="robots" content="noindex, nofollow" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <link rel="icon" href="/images/favicon/favicon.ico" />
-        <meta itemprop="name" content="" />
-        <meta itemprop="description" content="" />
-        <meta itemprop="image" content="" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/images/favicon/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/images/favicon/favicon-16x16.png"
-          sizes="16x16"
-        />
-        <link rel="manifest" href="/images/favicon/manifest.json" />
-        <link
-          rel="mask-icon"
-          href="/images/favicon/safari-pinned-tab.svg"
-          color="#ffffff"
-        />
-        <meta name="theme-color" content="#ffffff" />
+        <Meta />
+        <title>{title || "Ken Van Damme"}</title>
       </Head>
-      <section className={styles.flex}>{children}</section>
-    </>
+      <Header />
+      {children}
+      <Footer />
+    </Page>
   );
-}
+};
+
+export default Layout;
