376dff0982bdd2c0cce98bbdffa8d6c51b051c08
Solo244 <solo244@gmail.com>
2021-02-18 10:03:19 +0100

    Header component

--- components/header.js
+++ components/header.js
@@ -1,9 +1,46 @@
-import styles from "../styles/modules/header.module.css";
+// Dependencies
+import React from "react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+// Config/state
+import links from "../constants/links";
+// Components
+import Logo from "../images/logo";
+// Styles
+import { Link, Row, Col, Spacer, Text } from "@geist-ui/react";
 
-export default function Header({ children, style }) {
+const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className={`${styles.header} ${styles[style]}`}>
-      <section className={styles.container}>{children}</section>
-    </header>
+    <>
+      <Row gap={2} align="middle">
+        <Col>
+          <NextLink href="/">
+            <Link>
+              <Logo />
+            </Link>
+          </NextLink>
+        </Col>
+        <Col>
+          <Row justify="end">
+            {links.map(link => (
+              <NextLink href={link.path} key={link.path}>
+                {router.pathname === link.path ? (
+                  <Text b type="secondary">
+                    <Link block>{link.label}</Link>
+                  </Text>
+                ) : (
+                  <Link block>{link.label}</Link>
+                )}
+              </NextLink>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+      <Spacer y={3} />
+    </>
   );
-}
+};
+
+export default Header;
