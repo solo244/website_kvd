a9a034ba8010a5b00be0e3890ba4b0d9a95090c9
Solo244 <solo244@gmail.com>
2021-02-19 20:48:35 +0100

    Update index

--- pages/index.js
+++ pages/index.js
@@ -1,98 +1,80 @@
-import Head from "next/head";
-import Link from "next/link";
-import Layout from "../components/layout";
-import Sidebar from "../components/sidebar";
-import Content from "../components/content";
-import Footer from "../components/footer";
-import tileStyles from "../styles/modules/tile.module.css";
-import categoryStyles from "../styles/modules/category.module.css";
-import { getSortedWritingsData } from "../lib/writings";
-import { getSortedDesignsData } from "../lib/designs";
-import { getSortedProjectsData } from "../lib/projects";
-import { Calendar } from "react-feather";
+// Dependencies
+import React from "react";
+import NextLink from "next/link";
+// Config/state
+import designs from "../constants/designs";
+import writings from "../constants/writings";
+// Components
+import Intro from "../components/atoms/Intro";
+import Griditem from "../components/atoms/Griditem";
+import Block from "../components/atoms/Block";
+import Layout from "../components/molecules/Layout";
+// Styles
+import { Button, Row, Spacer, Grid, Description } from "@geist-ui/react";
+import User from "@geist-ui/react-icons/user";
+import Code from "@geist-ui/react-icons/code";
+import ImageIcon from "@geist-ui/react-icons/image";
 
-export default function Home({ data }) {
+const Homepage = () => {
   return (
-    <Layout home>
-      <Head>
-        <title>Ken Van Damme</title>
-      </Head>
-
-      <Sidebar>
-        <h1>Lorem all the ipsums</h1>
-        <p>
-          Webdesigner, webdeveloper, teacher, gamer, moron, realist,
-          procrastinator, fast talker, nerd, distracted by pretty colors. As a
-          profession, I make web-stuff at{" "}
-          <a href="https://www.mediasoft.be" className="link" target="_blank">
-            Mediasoft
-          </a>{" "}
-          and{" "}
-          <a href="https://www.weebit.be" className="link" target="_blank">
-            Weebit
-          </a>
-          .
-        </p>
-        <Link href="/about">
-          <a className={`button ${tileStyles.button}`}>More about me 😴</a>
-        </Link>
-        <Footer grid />
-      </Sidebar>
-
-      <Content grid>
-        {data.map(
-          ({ slug, blank, title, category, filters, date, background }) => {
-            return (
-              <article key={slug} className={tileStyles.tile}>
-                <Link href={`${slug}`}>
-                  <a
-                    className={`${tileStyles.tilelink} ${categoryStyles[category]}`}
-                    target={`${blank ? "_blank" : "_self"}`}
-                  >
-                    {background ? (
-                      <aside className={tileStyles.tileimage}>
-                        <img src={`images/designs/${background}`} alt={title} />
-                      </aside>
-                    ) : (
-                      <h4>
-                        {category}
-                        {filters && `: ${filters}`}
-                      </h4>
-                    )}
-                    <span className={tileStyles.tiledate}>
-                      <Calendar size="16" /> {date}
-                    </span>
-                    <h2>{title}</h2>
-                  </a>
-                </Link>
-              </article>
-            );
-          }
-        )}
-      </Content>
+    <Layout title="Ken Van Damme">
+      <Grid.Container gap={2} justify="center">
+        <Intro />
+        <Grid xs={24} sm={10} md={14} style={{ display: "block" }}>
+          <Spacer y={0.5} />
+          <Description title="My writing" />
+          {writings
+            .slice(0, 3)
+            .map(
+              writing =>
+                writing && (
+                  <Block
+                    key={writing.title}
+                    title={writing.title}
+                    type={writing.tag}
+                    intro={writing.intro}
+                    link={`/writings/${writing.link}`}
+                  />
+                )
+            )}
+          <Row justify="end">
+            <NextLink href="/writing">
+              <Button ghost type="secondary" auto>
+                All writing 📚
+              </Button>
+            </NextLink>
+          </Row>
+        </Grid>
+      </Grid.Container>
+      <Spacer y={3} />
+      <Grid.Container gap={2} justify="center">
+        <Griditem
+          title="About me"
+          link="about"
+          linktext="Learn more about me"
+          description="I like designing my own stuff for print"
+        >
+          <User />
+        </Griditem>
+        <Griditem
+          title="Coding stuff"
+          link="coding"
+          linktext="Look at my coding stuff"
+          description="I like designing my own stuff for print"
+        >
+          <Code />
+        </Griditem>
+        <Griditem
+          title="Designing for print"
+          link="designing"
+          linktext="Explore all my designs"
+          description="I like designing my own stuff for print"
+        >
+          <ImageIcon />
+        </Griditem>
+      </Grid.Container>
     </Layout>
   );
-}
+};
 
-export async function getStaticProps() {
-  let data = [];
-  const writings = getSortedWritingsData();
-  const designs = getSortedDesignsData();
-  const projects = getSortedProjectsData();
-  writings.map(writing => data.push(writing));
-  designs.map(design => data.push(design));
-  projects.map(project => data.push(project));
-
-  function dateToNum(d) {
-    d = d.split("/");
-    return Number(d[2] + d[1] + d[0]);
-  }
-
-  data.sort(function (a, b) {
-    return dateToNum(b.date) - dateToNum(a.date);
-  });
-
-  return {
-    props: { data: data },
-  };
-}
+export default Homepage;
