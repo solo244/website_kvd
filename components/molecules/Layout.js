5c7d8d724b07a0bbffbb6f6caf499af29bacdc93
Solo244 <solo244@gmail.com>
2021-02-20 21:09:26 +0100

    Build errors - molecules

--- components/molecules/Layout.js
+++ components/molecules/Layout.js
@@ -3,8 +3,8 @@ import React from "react";
 import Head from "next/head";
 // Components
 import Meta from "./NewMeta";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./NewHeader";
+import Footer from "./NewFooter";
 // Styles
 import { Page } from "@geist-ui/react";
 
