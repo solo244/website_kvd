d4a1446e1caeef610d1bbda346597507efe28a0e
Solo244 <solo244@gmail.com>
2021-02-20 20:57:31 +0100

    Added all designs

--- constants/designs.js
+++ constants/designs.js
@@ -1,5 +1,11 @@
 const designs = [
   {
+    title: "It's a trap",
+    image: "/images/designs/design37.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/t-shirt/It-s-a-trap-by-solo244/16493372.IJ6L0",
+  },
+  {
     title: "I like saying no",
     image: "/images/designs/design36.jpg",
     redbubble:
@@ -10,6 +16,120 @@ const designs = [
     image: "/images/designs/design35.jpg",
     redbubble:
       "https://www.redbubble.com/i/iphone-case/No-Dino-by-solo244/16170014.PGM2E",
+  },
+  {
+    title: "Choose your weapon",
+    image: "/images/designs/design34.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Choose-your-weapon-by-solo244/16068556.PGM2E",
+  },
+  {
+    title: "Keep calm and download...",
+    image: "/images/designs/design33.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Keep-calm-and-download-another-browser-by-solo244/16011083.PGM2E",
+  },
+  {
+    title: "I've missed you",
+    image: "/images/designs/design31.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/I-ve-missed-you-by-solo244/15890981.PGM2E",
+  },
+  {
+    title: "I feel the need for speed",
+    image: "/images/designs/design30.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/I-feel-the-need-the-need-for-speed-by-solo244/15817269.PGM2E",
+  },
+  {
+    title: "CTRL + ALT + DELETE",
+    image: "/images/designs/design29.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/When-all-else-fails-just-hold-CTRL-ALT-DELETE-by-solo244/15738973.PGM2E",
+  },
+  {
+    title: "Chameleon",
+    image: "/images/designs/design26.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Chameleon-by-solo244/15331529.PGM2E",
+  },
+  {
+    title: "Friends don't let friends...",
+    image: "/images/designs/design25.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Friends-don-t-let-friends-use-internet-explorer-by-solo244/15258745.PGM2E",
+  },
+  {
+    title: "May the WiFi be with you",
+    image: "/images/designs/design23.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/May-the-WiFi-be-with-you-by-solo244/15098323.PGM2E",
+  },
+  {
+    title: "All hail the president",
+    image: "/images/designs/design20.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/All-hail-the-president-by-solo244/14821710.PGM2E",
+  },
+  {
+    title: "Don't put all your eggs in...",
+    image: "/images/designs/design19.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Don-t-put-all-your-eggs-in-one-basket-by-solo244/14755541.PGM2E",
+  },
+  {
+    title: "I see dead pixels",
+    image: "/images/designs/design18.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/I-see-dead-pixels-by-solo244/14691278.PGM2E",
+  },
+  {
+    title: "Say hello to my little friend",
+    image: "/images/designs/design17.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Say-hello-to-my-little-friend-by-solo244/14629376.PGM2E",
+  },
+  {
+    title: "You want the algorithm",
+    image: "/images/designs/design15.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/You-want-the-algorithm-by-solo244/14529247.PGM2E",
+  },
+  {
+    title: "In Your Face",
+    image: "/images/designs/design14.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/In-Your-Face-by-solo244/14486341.PGM2E",
+  },
+  {
+    title: "I'll copy what he's pasting",
+    image: "/images/designs/design11.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/I-ll-copy-what-he-s-pasting-by-solo244/14230404.PGM2E",
+  },
+  {
+    title: "Not the quote you're look...",
+    image: "/images/designs/design10.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Not-the-quote-you-are-looking-for-by-solo244/14106891.PGM2E",
+  },
+  {
+    title: "Real Pixels Have Curves",
+    image: "/images/designs/design9.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Real-Pixels-Have-Curves-by-solo244/13930630.PGM2E",
+  },
+  {
+    title: "Digging for a poem",
+    image: "/images/designs/design8.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Digging-for-a-poem-by-solo244/13079267.PGM2E",
+  },
+  {
+    title: "Trust me I'm a doctor",
+    image: "/images/designs/design7.jpg",
+    redbubble:
+      "https://www.redbubble.com/i/iphone-case/Trust-me-I-m-a-doctor-by-solo244/13028773.PGM2E",
   },
 ];
 
