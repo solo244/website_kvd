248204c1be5375eac3aae5d6bc9fb786fa9009e7
Solo244 <solo244@gmail.com>
2021-04-21 09:13:40 +0200

    Update timeline

--- constants/timeline.js
+++ constants/timeline.js
@@ -6,80 +6,73 @@ const timeline = [
   },
 
   {
-    description:
-      "Saw the light and came into existence. It was kind of a big mess to be honest.",
-    year: "1990",
-  },
-  {
     description: "First steps were made.",
-    year: "1995",
+    year: "1990",
   },
   {
     description:
       "The family got a new computer and I was officially stuck to it from then on.",
-    year: "1996",
+    year: "1995",
   },
   {
     description: "Early goal found to be a professional Playstation player.",
-    year: "1997",
+    year: "1996",
   },
   {
     description: "Fell in love with Space Jam.",
-    year: "1999",
+    year: "1997",
   },
   {
     description: "Was totally unprepared for Y2K.",
-    year: "2000",
+    year: "1999",
   },
   {
     description:
       "Switched school and found new dreams of what to be when I grow up.",
+    year: "2000",
+  },
+  {
+    description:
+      "Organised a waffle sale to fund a couple of computers and start an official class in school to learn how to use them. Then decided together with my friends that playing games is good for your mental health.",
     year: "2002",
   },
 
   ,
   {
-    description:
-      "Organised a funding waffle sale to finance a couple of computers and start an official class in school to learn to use them. Then decided together with my friends that playing games is good for your mental health.",
+    description: "Started playing basketball in competition.",
     year: "2003",
   },
 
   {
-    description: "Started playing basketball in competition.",
+    description: "Decided school wasn't for me and quit at the age of 17.",
     year: "2005",
   },
 
   {
-    description: "Decided school wasn't for me and quit at the age of 17.",
+    description: "Tried my own way to work and get educated.",
     year: "2006",
   },
 
   {
-    description: "Tried my own way to work and get educated.",
+    description: "Moved and started a company of my own.",
     year: "2007",
   },
 
   {
-    description: "Moved and started a company of my own.",
+    description: "Started a course on gamedesign.",
     year: "2008",
   },
 
   {
-    description: "Started a course on gamedesign.",
-    year: "2009",
-  },
-
-  {
     description:
-      "Realised I love playing games, not making them. Company up and running - clearly more for me than Uni.",
-    year: "2010",
+      "Realised I love playing games, not making them. My own company up and running - clearly more for me than Uni.",
+    year: "2009",
   },
 
   {
     description: "Bought my first car.",
     year: "2010",
   },
-
   {
     description: "Moved again.",
     year: "2010",
@@ -92,18 +85,18 @@ const timeline = [
 
   {
     description: "Bought my second car.",
-    year: "2012",
+    year: "2011",
   },
 
   {
     description: "Started teaching adults.",
-    year: "2014",
+    year: "2012",
   },
 
   {
     description:
       "Still working, still teaching and now finally smug enough to make my own tutorials.",
-    year: "2015",
+    year: "2014",
   },
 
   {
@@ -113,25 +106,26 @@ const timeline = [
 
   {
     description: "Got completely obsessed with Dungeons & Dragons.",
-    year: "2016",
+    year: "2015",
   },
 
   {
     description: "Extended the family by one cat.",
-    year: "2017",
+    year: "2016",
   },
 
   {
     description: "Found a new job at Yappa.",
+    year: "2017",
+  },
+  {
+    description: "Found our own little house to rent.",
     year: "2018",
   },
-
   {
-    description:
-      "Found our own little house to rent and cat number two joins the family.",
+    description: "Cat number two joins the family.",
     year: "2019",
   },
-
   {
     description: "Switched to Mediasoft.",
     year: "2020",
